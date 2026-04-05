8d936168008660a1b4888a2f36e4fff18da7f3f0
POSTTTT <post906@gmail.com>
2026-04-05 18:47:22 +0900

    new jsx dashboard
    - test new dashboard using .jsx
    - set up node.js and other library

---
+++ main.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import SmartShelfDashboard from './smart_shelf_dashboard';
+
+ReactDOM.createRoot(document.getElementById('root')).render(<SmartShelfDashboard />);
