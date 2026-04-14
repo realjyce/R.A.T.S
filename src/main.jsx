353cef1d535d72bc86e1e99cfcf6c4cb15423559
POSTTTT <post906@gmail.com>
2026-04-14 15:20:42 +0900

    re-arrange file directory

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import SmartShelfDashboard from './smart_shelf_dashboard';
+import SmartShelfDashboard from './SmartShelfDashboard';
 
 ReactDOM.createRoot(document.getElementById('root')).render(<SmartShelfDashboard />);
